1ad12da98a2706a06e26a735627ebe8956d0ab18
20210699 <20210699@duksung.ac.kr>
2026-06-29 20:31:38 +0900

    chore: 프로젝트 초기 설정 및 의존성 추가
    - Spring Boot 3.5.16, Java 21 설정
    - PostgreSQL + PostGIS, JPA, hibernate-spatial 추가
    - OAuth2 Client, JWT 의존성 추가
    - validation, springdoc-openapi 추가
    - actuator(모니터링) 추가

---
+++ src/main/java/cmc/rodi/RodiApplication.java
@@ -0,0 +1,13 @@
+package cmc.rodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RodiApplication.class, args);
+    }
+
+}
